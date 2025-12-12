15ef120a857d6d0b25d960ba10791737bb995e0f
Syed-Hamza13 <syed13hamza@gmail.com>
2025-12-12 23:45:56 +0530

    adding marked down

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/global.css';
 import App from './pages/App.jsx';
-
+const a = 42;
+console.log('The answer is', a);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
 
